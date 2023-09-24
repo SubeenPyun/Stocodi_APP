50d60cd0dfaef6eb223ea26e76bb93accc83b3ff
SubinPyeon <sbp111@naver.com>
2023-09-25 00:25:48 +0900

    Revert "modify teacher comment api"
    This reverts commit 6fe62ed0d6299228815fd4c695ca7485a6ba8404.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:logger/logger.dart';
-import 'package:dio/dio.dart';
 
 void main() {
   runApp(const MyApp());
-  // https://dev-yakuza.posstree.com/ko/flutter/http/
 }
 
 class MyApp extends StatelessWidget {
@@ -19,22 +15,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         // This is the theme of your application.
         //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a blue toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
         // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const MyHomePage(),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -45,46 +48,24 @@ class MyHomePage extends StatelessWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  void _callAPI() async {
-    var logger = Logger(
-      printer: PrettyPrinter(),
-    );
+  final String title;
 
-    var url = Uri.parse(
-      'http://49.50.175.59:8080/api/v1/users/nickname/exists?nickname=예리미양',
-    );
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
 
-    var response = await http.get(url);
-    print('Response status: ${response.statusCode}');
-    print('Response body: ${response.body}');
-    logger.d('print logger success');
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
 
-    // var url = Uri.parse(
-    //   'http://49.50.175.59:8080/api/v1/users/email/exists?email=rim0399@naver.com',
-    // );
-    // var response = await http.get(url);
-    // print('Response status: ${response.statusCode}');
-    // print('Response body: ${response.body}');
-
-    // var url = Uri.parse('http://49.50.175.59:8080/api/v1/users/register');
-    // print(['USER'].join('').runtimeType);
-    // var response = await http.post(url,
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //       "alg": "HS256",
-    //     },
-    //     body: jsonEncode(
-    //     {
-    //     'email':'test@naver.com',
-    //     'name':'편수빈',
-    //     'password':'1234',
-    //     'nickname':'봉봉',
-    //     'birth_date':'2001-07-03',
-    //     'interest_categories':'IT',
-    //     'roles':['USER'].join(''),
-    //   }));
-    // print('Response status: ${response.statusCode}');
-    // print('Response body: ${response.body}');
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
   }
 
   @override
@@ -97,18 +78,48 @@ class MyHomePage extends StatelessWidget {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        // TRY THIS: Try changing the color here to a specific color (to
+        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+        // change color while the other colors stay the same.
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text('api Example'),
+        title: Text(widget.title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-         child: ElevatedButton(
-          onPressed:_callAPI,
-          child: const Text('Call API'),
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          //
+          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
+          // action in the IDE, or press "p" in the console), to see the
+          // wireframe for each widget.
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
