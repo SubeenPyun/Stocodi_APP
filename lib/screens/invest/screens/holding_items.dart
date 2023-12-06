089632a5e65cb10215de7f4fef276ceb712eb755
kimye702 <kimye702@naver.com>
2023-12-06 23:38:40 +0900

    [REFACTOR] holding_items 괄호 추가

--- lib/screens/invest/screens/holding_items.dart
+++ lib/screens/invest/screens/holding_items.dart
@@ -38,6 +38,7 @@ Container _bodyWidget(BuildContext context){
     "Ionic": 2,
   };
 
+
   List<String> keys = dataMap.keys.toList();
   List<double> values=dataMap.values.map<double>((value) => value as double).toList();
   double S = values.fold(0, (previousValue, element) => previousValue + element);
