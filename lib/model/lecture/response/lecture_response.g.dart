8148831c1754c0e281734e7039fa05d54e524d0f
DongGi-create <84391428+DongGi-create@users.noreply.github.com>
2023-12-29 01:33:15 +0900

    [Refactor]change lecture

---
+++ lib/model/lecture/response/lecture_response.g.dart
@@ -0,0 +1,29 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'lecture_response.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+LectureResponse _$LectureResponseFromJson(Map<String, dynamic> json) =>
+    LectureResponse(
+      id: json['id'] as int,
+      video_link: json['video_link'] as String,
+      title: json['title'] as String,
+      description: json['description'] as String,
+      tags: (json['tags'] as List<dynamic>).map((e) => e as String).toList(),
+      views: json['views'] as int,
+      likes: json['likes'] as int,
+    );
+
+Map<String, dynamic> _$LectureResponseToJson(LectureResponse instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'video_link': instance.video_link,
+      'title': instance.title,
+      'description': instance.description,
+      'tags': instance.tags,
+      'views': instance.views,
+      'likes': instance.likes,
+    };
